f433bdf97cbb391b65563474d9c8513e54bb57a1
gcockroft <77020027+gcockroft@users.noreply.github.com>
2022-08-20 23:43:30 -0400

    timer has state now mf

--- src/components/TimeCard/Timer.view.js
+++ src/components/TimeCard/Timer.view.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 
 class Timer extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     running: false,
-  //   };
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      running: false,
+      time: new Date(),
+    };
+  }
 
-  start() {}
+  // Changes state of the timer. Calls timer start or stop methods depending on new state.
+  onPress = () => {
+    this.state.running = !this.state.running;
+    console.log(this.state.running);
+    this.state.running ? this.start() : this.stop();
+  };
+
+  stop = () => {
+    console.log('stopped timer');
+  };
+
+  start = () => {
+    console.log('started timer');
+  };
 
   render() {
-    console.log(this.props.running);
+    console.log(this.state.running);
     return (
-      <TouchableOpacity>
-        <Text>0:00 {this.props.running}</Text>
+      <TouchableOpacity onPress={this.onPress}>
+        <Text>{this.state.time.toLocaleTimeString()}</Text>
       </TouchableOpacity>
     );
   }
